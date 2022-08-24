8871684ccd251d8bd2dff4cfe1e75f832e1fa2ad
Harishk820 <harishk820@gmail.com>
2022-08-24 18:09:51 +0530

    Update Task.js

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 function Task() {
